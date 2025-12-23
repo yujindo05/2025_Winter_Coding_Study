ac2fbda7a5796af428dfe34e275ef2b8013a94d1
rudcoco <rudcoco09@naver.com>
2025-12-24 00:27:36 +0900

    Feat: Solve 10951

---
+++ rudcoco/2025_12/Week05/1223_10951.py
@@ -0,0 +1,9 @@
+def main():
+    while True:
+        try:
+            a, b = map(int, input().split())
+            print(a + b)
+        except:
+            break
+if __name__ == '__main__':
+    main()
