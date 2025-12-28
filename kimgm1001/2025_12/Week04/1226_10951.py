5edda7a30151d039d6d091368be3949478b608d6
kimgm1001 <kimgm1001@gmail.com>
2025-12-28 20:17:45 +0900

    Feat: Solve 10951

---
+++ kimgm1001/2025_12/Week04/1226_10951.py
@@ -0,0 +1,11 @@
+def main():
+
+    while True:
+        try:
+            a, b = map(int, input().split())
+            print(a + b)
+        except:
+            break
+
+if __name__ == '__main__':
+    main()
