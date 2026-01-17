df3e731d0c53a413d8dd2bf80658495401f136a3
kimgm1001 <kimgm1001@gmail.com>
2026-01-17 22:49:21 +0900

    Feat: Solve 1920

---
+++ kimgm1001/2026_01/Week02/0107_1920.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+arr1 = set(map(int, input().split()))
+
+m = int(input())
+arr2 = list(map(int, input().split()))
+
+for num in arr2:
+    if num in arr1:
+        print(1)
+    else:
+        print(0)
