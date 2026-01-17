89da6c0e549ff737f128ccb02018bb0374bec4c6
kimgm1001 <kimgm1001@gmail.com>
2026-01-17 22:49:08 +0900

    Feat: Solve 11650

---
+++ kimgm1001/2026_01/Week02/0107_11650.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+arr = []
+
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+
+arr.sort(key = lambda x : (x[0], x[1]))
+
+for i in arr:
+    print(*i)
