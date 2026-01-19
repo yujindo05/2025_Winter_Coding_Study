30d4b302e805ae0292dcccf9f629914999f82a31
yujin <ureal@g.cnu.ac.kr>
2026-01-19 22:40:19 +0900

    Refactor main function in 0109_10814.py
    Refactor main function to improve readability and efficiency.

--- yujin05/2026_01/week02/0109_10814.py
+++ yujin05/2026_01/week02/0109_10814.py
@@ -1,19 +1,1 @@
-def main():
 
-    n = int(input())
-    people = []
-
-    for i in range(n):
-        data = input().split()
-        people.append ([int(data[0]),data[1]])
-
-    for age in range(1,201):
-        for person in people:
-            p_age = person[0] 
-            p_name = person[1]
-
-            if p_age == age:
-                print(p_age,p_name)
-
-if __name__ == '__main__':
-    main()
