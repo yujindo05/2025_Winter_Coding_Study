4d006c1e9b497b6d89c9e2208520b54ecf1b31e8
yujin <ureal@g.cnu.ac.kr>
2026-01-19 22:40:37 +0900

    Update 0109_11050.py

--- yujin05/2026_01/week02/0109_11050.py
+++ yujin05/2026_01/week02/0109_11050.py
@@ -1,14 +1,1 @@
-def main():
 
-    n, k = map(int,input().split())
-
-    def factorial(x):
-        a = 1
-        for i in range(2, x+1):
-            a *= i
-        return a
-
-    print(factorial(n)//(factorial(k) * factorial(n-k)))
-
-if __name__ == '__main__':
-    main()
